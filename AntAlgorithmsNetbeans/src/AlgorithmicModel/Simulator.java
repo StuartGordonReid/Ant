4abd0481819c62d884fea8165138563af73ab6de
Simon van Dyk <simon.vandyk@gmail.com>
2013-04-20 08:22:57 +0200

    Commiting broken code for Stuart to see.

--- AntAlgorithmsNetbeans/src/AlgorithmicModel/Simulator.java
+++ AntAlgorithmsNetbeans/src/AlgorithmicModel/Simulator.java
@@ -11,19 +11,9 @@ package AlgorithmicModel;
 public class Simulator {
 
     public static void main(String args[]) {
-        int iterations = 50;
-        AntAlgorithm ants = new AntAlgorithm(10, 50, 15);
+        int iterations = 100;
+        AntAlgorithm ants = new AntAlgorithm(10, 20, 5);
         
-        ants.getGrid().printGrid();
-        
-        for (int i = 0; i < iterations; ++i){
-            ants.moveAnts();
-            if (i % 1 == 0){
-                ants.getGrid().printGrid();
-                System.out.println();
-            }
-        }
-        
-        ants.getGrid().printGrid();
+        ants.run(iterations);
     }
 }
