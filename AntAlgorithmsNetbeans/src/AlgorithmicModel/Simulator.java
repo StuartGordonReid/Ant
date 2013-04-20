52948bcb8f3c4345cbff9045b57808b5a46f86f1
Stuart Reid <stuart@sinkwa.com>
2013-04-20 14:28:40 +0200

    Updated parameters

--- AntAlgorithmsNetbeans/src/AlgorithmicModel/Simulator.java
+++ AntAlgorithmsNetbeans/src/AlgorithmicModel/Simulator.java
@@ -11,13 +11,22 @@ package AlgorithmicModel;
 public class Simulator {
 
     public static void main(String args[]) {
-        int iterations = 100000;
-        AntAlgorithm ants = new AntAlgorithm(100, 100, 15);
-        int numitems   = 700;
-        int numants    = 50;
-        int memorysize = 0;
-        AntAlgorithm ants = new AntAlgorithm(gridsize, numitems, numants, memorysize);
 
-        ants.run(iterations,500);
+        int gridSize = 50;
+        int iterations = 100000;
+        int numitems = (int) (gridSize * gridSize) / 20;
+        int numants = (int) numitems / 10;
+        int memorysize = 0;
+        int resolution = 1000;
+
+        System.out.println("Starting simulation with: \n"
+                + "Iterations: " + iterations + "\n"
+                + "Grid Size: " + gridSize + "x" + gridSize + "\n"
+                + "Number of items (5% of GridSize): " + numitems + "\n"
+                + "Number of ants (1 Ant: 10 Items): " + numants + "\n"
+                + "Memory Size: " + memorysize);
+        
+        AntAlgorithm ants = new AntAlgorithm(gridSize, numitems, numants, memorysize);
+        ants.run(iterations, resolution);
     }
 }
